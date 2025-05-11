22396690a099678d55d92898108eeab1caa87498
AngelBnd <angelabenedictins@gmail.com>
2025-05-11 17:46:29 +0700

    fixed button again

--- src/components/Layout/SidePanel.jsx
+++ src/components/Layout/SidePanel.jsx
@@ -5,7 +5,7 @@ import GameButtons from './GameButtons';
 
 export default function SidePanel(){
     return(
-        <div className="container side-panel d-flex flex-column justify-content-center align-items-center gap-5 p-0 m-0">
+        <div className="container side-panel d-flex flex-column justify-content-center align-items-center gap-4 p-0 m-0">
             <Location/>
             <Inventory/>
             <GameButtons/>
