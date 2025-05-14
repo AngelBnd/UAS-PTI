346dde6b54cc6f745f25145aa52be1a0d5e5e045
WizeOnOfficial <wizeongaming2019@gmail.com>
2025-05-14 22:52:42 +0700

    added wasd for movement

--- src/components/Layout/GameArea.jsx
+++ src/components/Layout/GameArea.jsx
@@ -39,49 +39,6 @@ export default function GameArea() {
     const[velocity, setVelocity] = useState({x:0,y:0});
 
     const collidableObjectsRefs = [planetRefs, itemRefs];
-
-    useEffect(()=>{
-        const handleKeyDown = (e)=>{
-            setVelocity((prev)=>{
-                switch(e .key){
-                case 'ArrowRight' : case 'd' :
-                    return { ...prev, x: -2 };
-                case 'ArrowLeft' : case 'a' :
-                    return { ...prev, x: 2 };
-                case 'ArrowUp' : case 'w' :
-                    return { ...prev, y: 2 };
-                case 'ArrowDown' : case 's' :
-                    return { ...prev, y: -2 };
-                default:
-                    return prev;
-                }
-            }); 
-        };
-
-        const handleKeyUp = (e)=> {
-            setVelocity((prev)=>{
-                switch(e.key){
-                    case 'ArrowRight' : case 'd' :
-                    case 'ArrowLeft' : case 'a' :
-                        return{...prev, x: 0 };
-                    case 'ArrowUp' : case 'w' :
-                    case 'ArrowDown' : case 's' :
-                        return{...prev, y: 0};
-                    default:
-                        return prev;
-                }
-            });
-        };  
-
-        window.addEventListener('keydown', handleKeyDown);
-        window.addEventListener('keyup', handleKeyUp);
-
-        return()=>{
-            window.removeEventListener('keydown', handleKeyDown);
-            window.removeEventListener('keyup', handleKeyUp);
-        }
-
-    }, []);
   
     useMovement(setVelocity);
 
