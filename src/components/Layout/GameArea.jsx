8859163e5fed31c2be9bb72772c1f07cab67fc33
AngelBnd <angelabenedictins@gmail.com>
2025-05-22 19:45:44 +0700

    modularized pick uo

--- src/components/Layout/GameArea.jsx
+++ src/components/Layout/GameArea.jsx
@@ -312,34 +312,10 @@ export default function GameArea({ setLocation, saveplayerLocation, saveplanetLo
                             } else {
                                 if(ItemsInInventory.length == 6) return;
                                 else {
-                                    const targetItem = collisionInfos.collidedItem;
-                            
-                                    if(!targetItem)return;
-
-                                    setItemsInInventory(prevItemsInv => [...prevItemsInv, targetItem]);
-                                    const targetId = targetItem?.id;
-
-
-                                    collisionInfos.cool = false;   
-                                    collisionInfos.collidedItem = null;
-                                    collisionInfos.showed = 0;
-                                    
-
-                                    if (targetId !== undefined) {
-                                        setItemsOnMap((prevItems) => {
-                                            const itemIndex = prevItems.findIndex((item) => item.id === targetId);
-                                            prevItems[itemIndex] = "";
-                                            itemRefs.current[itemIndex] = "";
-                                            return prevItems;
-                                        });
-                                    }
-                                }
-
-        
-                        
+                                    handlePickUpItem(collisionInfos.collidedItem, collisionInfos, itemRefs, setItemsInInventory, setItemsOnMap);            
                             }
                             
-                        }}
+                        }}}
                         >
                         {(() => {
                             if (collisionInfos.collidedLocation) {
